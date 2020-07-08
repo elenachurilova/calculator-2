1c991ccb2724ba943482baffdf564f047fad6d97
Elena Churilova <ellena.churilova@gmail.com>
2020-07-08 16:04:12 -0700

    Addition functionality

--- calculator.py
+++ calculator.py
@@ -1,7 +1,24 @@
 """CLI application for a prefix-notation calculator."""
 
-from arithmetic import (add, subtract, multiply, divide, square, cube,
-                        power, mod, )
+# from arithmetic import (add, subtract, multiply, divide, square, cube,
+#                         power, mod, )
 
+
+while True:
+
+    user_input = input("Please enter your equasion > ")
+
+    tokens = user_input.split(" ")
+
+    func = tokens[0]
+    num1 = tokens[1]
+    num2 = tokens[2]
+
+    if tokens[0] == "q":
+        print("You requested to exit...")
+
+    else:
+        if func == "+":
+            print((float(num1)) + (float(num2)))
 
 # Replace this with your code
